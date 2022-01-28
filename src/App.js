1aadacb71359c93c5be459a956fe0c3e07106744
Gaurav <gauravsing1195@gmail.com>
2022-01-29 01:51:58 +0530

    Section3 Added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { Header } from './Header/Header';
 import {Section1} from "./Sections/Section1"
 import {Section2} from "./Sections/Section2"
-
+import {Section3} from "./Sections/Section3"
 function App() {
   return (
     <div className="App">
      <Header/>
      <Section1/>
      <Section2/>
+     <Section3/>
     </div>
   );
 }
---
+++ src/Sections/Section3.js
@@ -0,0 +1,212 @@
+import React from 'react';
+import styled from "styled-components"
+import Stripe3 from "../Images/Stripe3.png"
+import HalfStar from "../Images/HalfStar.png"
+import Star from "../Images/Star.png"
+import Picture from "../Images/Picture (3).png"
+import Left from "../Images/Left.png"
+import Right from "../Images/Right.png"
+import Vertical from "../Images/Vertical.png"
+
+export const Section3 = () => {
+  return (
+      <Container>
+                   <Head>
+                   Trusted by students <br/><img className='Stripe' src={Stripe3}/> accross nation
+                   </Head>
+                   <Reviews>
+                       <img src={Star} />
+                       <img src={Star} />
+                       <img src={Star} />
+                       <img src={Star} />
+                       <img src={HalfStar} />
+                       <Rating>
+                        
+                        4.5 of 5
+                        
+                        <Text>Based on 1000+ Reviews revieved</Text>
+                       </Rating>
+                        
+  
+                   </Reviews>
+                   <CardSection>
+                   <Card>
+                       <Heading>
+                       Efficient Collaborating
+                      </Heading>
+                      <Content>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed imperdiet nibh lectus feugiat nunc sem.
+                      </Content>
+                      <Profile>
+                        <img src={Picture }/>
+                          <Post>
+                          Jane Cooper
+                          <h5>
+                          CEO at ABC Corporation
+                          </h5>
+                          </Post>
+                      </Profile>
+
+                   </Card>
+                   <Card>
+                   <Heading>
+                       Efficient Collaborating
+                      </Heading>
+                      <Content>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed imperdiet nibh lectus feugiat nunc sem.
+                      </Content>
+                     <Profile>
+                        <img src={Picture }/>
+                          <Post>
+                          Jane Cooper
+                          <h5>
+                          CEO at ABC Corporation
+                          </h5>
+                          </Post>
+
+
+                     </Profile>
+                   </Card>
+
+                   </CardSection>
+                   <Proceed>
+                   <Button>
+                         <img src={Left}/>
+                    </Button>
+                    <img src={Vertical}/>
+                    <Button>
+                     <img src={Right}/>
+                    </Button>
+                   </Proceed>
+                   
+                  
+      </Container>
+
+
+  );
+};
+
+const Container=styled.div`
+height: 1054px;
+
+width: 1ref;
+border-radius: 0px;
+background: #041562;
+position: relative;
+background: #FFF89A;
+
+
+`;
+const Head=styled.div`
+font-family: DM Sans;
+font-size: 78px;
+font-style: normal;
+font-weight: 700;
+line-height: 101px;
+letter-spacing: 0em;
+text-align: center;
+padding-top:70px;
+img{
+    position:absolute;
+    margin-left:70px;
+
+}
+.Stripe{
+    padding-left:60px;
+}
+
+`;
+const Reviews=styled.div`
+img{
+    margin:10px 10px;
+}
+display:flex;
+justify-content:center;
+`;
+const Rating=styled.div`
+
+font-family: DM Sans;
+font-style: normal;
+font-size: 28px;
+text-align:left;
+font-weight: bold;
+margin-left:20px;
+`;
+const Text=styled.div`
+font-family: DM Sans;
+font-style: normal;
+font-weight: normal;
+
+`;
+const CardSection=styled.div`
+
+display:flex;
+justify-content:space-evenly;
+margin-top:98px;
+`;
+const Card=styled.div`
+height: 256px;
+width: 508px;
+left: 403px;
+top: 456px;
+border-radius: 10px;
+background: #FFC900;
+padding-top:38px;
+
+`;
+const Heading=styled.div`
+font-family: DM Sans;
+font-size: 36px;
+font-style: normal;
+font-weight: 700;
+line-height: 32px;
+letter-spacing: 0em;
+text-align: center;
+
+
+
+`;
+const Content=styled.div`
+padding-top:32px;
+font-family: DM Sans;
+font-size: 24px;
+font-style: normal;
+font-weight: 400;
+line-height: 29px;
+letter-spacing: 0em;
+text-align: center;
+
+
+`;
+const Post=styled.div`
+font-family: DM Sans;
+font-size: 28px;
+font-style: normal;
+font-weight: 700;
+line-height: 28px;
+letter-spacing: 0em;
+text-align: center;
+h5{
+    margin-top:-1px;
+font-family: DM Sans;
+font-style: normal;
+font-weight: normal;
+font-size: 18px;
+color:#474A57;
+}
+
+
+`;
+const Profile=styled.div`
+margin-top:50px;
+
+`;
+
+const Proceed=styled.div`
+margin-top:150px;
+`;
+const Button =styled.button`
+
+background:transparent;
+border:none;
+`;
